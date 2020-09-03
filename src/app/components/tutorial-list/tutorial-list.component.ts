5c4c3adb6454280d1f07f0d6ff714f58463724df
sarlury <jemy.sarlury@gmail.com>
2020-09-03 14:17:32 +0700

    tutorial list

--- src/app/components/tutorial-list/tutorial-list.component.ts
+++ src/app/components/tutorial-list/tutorial-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from '../../services/tutorial.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tutorial-list',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialListComponent implements OnInit {
 
-  constructor() { }
+  tutorials: any;
+  currentTutorial = null;
+  currentIndex = -1;
+  title = '';
+
+  constructor(
+    private tutorialService: TutorialService
+  ) { }
 
   ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  refreshList():void {
+    this.currentTutorial = null;
+    this.currentIndex = -1;
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials():void {
+    this.tutorialService.getAll().snapshotChanges()
+        .pipe(
+            map(change => change.map(c => ({
+              key: c.payload.key, ...c.payload.val()
+            })
+          )
+        )
+      ).subscribe(data => {
+        this.tutorials = data;
+      });
+  }
+
+  setActiveTutorial(tutorial, index): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .then(() => this.refreshList())
+        .catch(err => console.log(err));
   }
 
 }
